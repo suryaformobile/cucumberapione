9d232f39cdf63c133948e9835e309ea8da066f03
Surya <Surya@DESKTOP-HCQ1ED9>
2021-12-29 09:21:33 +0530

    accesstoken

--- Cucumberapifw/src/test/java/Utility/Auth.java
+++ Cucumberapifw/src/test/java/Utility/Auth.java
@@ -3,7 +3,7 @@ package Utility;
 public class Auth {
 
 
-	public final static String bearer_token= "ghp_GNEwJSURD3Ir8Yrb2uPNxP2Vo9eTyu1kq8T9";
+	public final static String bearer_token= "public accesstokenvalue";
 	public static String getBearertoken(){
 		//just
 		return bearer_token;
