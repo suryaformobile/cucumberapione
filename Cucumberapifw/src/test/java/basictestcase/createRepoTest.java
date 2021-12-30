98dbd30763c16aa411016b10d3988e848bb974a7
Surya <Surya@DESKTOP-HCQ1ED9>
2021-12-30 15:19:27 +0530

    getting some faster jackson deserialized error

--- Cucumberapifw/src/test/java/basictestcase/createRepoTest.java
+++ Cucumberapifw/src/test/java/basictestcase/createRepoTest.java
@@ -1,6 +1,9 @@
 package basictestcase;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,10 +14,11 @@ import Utility.Auth;
 import Utility.createURL;
 import Utility.fetchvaluefromjson;
 import Utility.payloadGenerator;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class createRepoTest {
-	
+	public static JsonPath jsonPath;
 	Response response;
 //	@Test
 	public void createrepoTestcase() throws IOException {
@@ -24,7 +28,7 @@ public class createRepoTest {
 		System.out.println("start2");
 		String bearer_Token = Auth.getBearertoken();
 		//System.out.println(bearer_Token);
-		response = Baseapiclass.postRequest1(endpoint,req_payLoad,bearer_Token);
+		response = Baseapiclass.postRequest(endpoint,req_payLoad,bearer_Token);
 		System.out.println("start3");
 		String responseString=response.getBody().asString();
 		System.out.println(responseString);
@@ -33,9 +37,9 @@ public class createRepoTest {
 	
 	//@Test
 	public void getlocalapiTestcase() throws IOException {
-		String endpoint=createURL.getBaseURI("users");
+		String endpoint=createURL.getBaseURI("status");
 		//System.out.println(endpoint);
-		String req_payLoad=payloadGenerator.generateStringPayload("justuserjson.json");
+		//String req_payLoad=payloadGenerator.generateStringPayload("justuserjson.json");
 		//System.out.println("start2");
 		response =Baseapiclass.getRequest(endpoint);
 		String responseString=response.getBody().asString();
@@ -45,7 +49,7 @@ public class createRepoTest {
 		//String exp=fetchvaluefromjson.valuefromjson(responseString,"laststname");
 		//String act=fetchvaluefromjson.valuefromjson(req_payLoad,"name");
 		
-		System.out.println(fetchvaluefromjson.valuefromjson(responseString,"laststname"));
+		System.out.println(fetchvaluefromjson.valuefromjson(responseString,"hit"));
 		//System.out.println(act);
 		Assert.assertEquals(200, response_statuscode);
 		//Assert.assertEquals(exp, act);
@@ -53,28 +57,81 @@ public class createRepoTest {
 }
 	
 	
-	//@Test
+	@Test
 	public void postlocalapiTestcase() throws IOException {
-		String endpoint=createURL.getBaseURI("users");
+		String endpoint=createURL.getBaseURI("status");
 		System.out.println(endpoint);
-		String req_payLoad=payloadGenerator.generateStringPayload("justuserjson.json");
+		String req_payLoad=payloadGenerator.generateStringPayload("actions.json");
 		System.out.println("start2");
 		response =Baseapiclass.postRequest(endpoint, req_payLoad);
 		String responseString=response.getBody().asString();
-		//System.out.println(responseString);
-		//int response_statuscode=response.getStatusCode();
+		System.out.println(responseString);
+		int response_statuscode=response.getStatusCode();
 		//Assert.assertEquals(201, response_statuscode);
 		
-		//String exp=fetchvaluefromjson.valuefromjson(responseString,"laststname");
-		//String act=fetchvaluefromjson.valuefromjson(req_payLoad,"laststname");
+		String exp=fetchvaluefromjson.valuefromjson(responseString,"status[1].hit3");
+		String act=fetchvaluefromjson.valuefromjson(req_payLoad,"status[1].hit3");
 		
-		System.out.println(fetchvaluefromjson.valuefromjson(responseString,"laststname"));
-		System.out.println(fetchvaluefromjson.valuefromjson(req_payLoad,"laststname"));
+		System.out.println("trying to fer list of 1st array");
 		
-		//Assert.assertEquals(exp, act);
+	//notworking throwing jackson error	Getting can't deserialized
+		    List<Object> allstatus = response.as(List.class);
+		    Map<String,Object>  letmecheck= (Map<String,Object>)allstatus.get(0);
+		    System.out.println(letmecheck.get("hit3"));
+		//jackson 
+			
+		//String act1=fetchvaluefromjson.valuefromjson(req_payLoad,"status[0]");//throwing error
+		//System.out.println(act1);
+						
+		System.out.println(exp);
+		System.out.println(act);
 		
-		Assert.assertEquals(fetchvaluefromjson.valuefromjson(responseString,"name"), fetchvaluefromjson.valuefromjson(req_payLoad,"name"));
+		JsonPath extractor= response.jsonPath();
+		ArrayList<String> statuses=  extractor.get("status[0]");
+		System.out.println(statuses.size());//fetching data
+		
+		
+		
+		System.out.println(statuses.get(0));//throwing error
+	
+		
+//		for (int i=0;i<=statuses.size();i++)
+//		{
+//			System.out.println(statuses.get(i));
+//			
+//		}
+		//System.out.println(fetchvaluefromjson.valuefromjson(responseString,"laststname"));
+		//System.out.println(fetchvaluefromjson.valuefromjson(req_payLoad,"laststname"));
+		
+		Assert.assertEquals(exp, act);
+		
+		//Assert.assertEquals(fetchvaluefromjson.valuefromjson(responseString,"name"), fetchvaluefromjson.valuefromjson(req_payLoad,"name"));
 
 }
+	
+	//@Test
+		public void putlocalapiTestcase_object() throws IOException {
+			String endpoint=createURL.getBaseURI("");
+			System.out.println(endpoint);
+			String req_payLoad=payloadGenerator.generateStringPayload("actions.json");
+			System.out.println(req_payLoad);
+			response =Baseapiclass.putRequest(endpoint, req_payLoad);
+			String responseString=response.getBody().asString();
+			System.out.println(responseString);
+			int response_statuscode=response.getStatusCode();
+			Assert.assertEquals(201, response_statuscode);
+			
+			//String exp=fetchvaluefromjson.valuefromjson(responseString,"laststname");
+			//String act=fetchvaluefromjson.valuefromjson(req_payLoad,"laststname");
+			
+			//System.out.println(fetchvaluefromjson.valuefromjson(responseString,"hit1"));
+			//System.out.println(fetchvaluefromjson.valuefromjson(req_payLoad,"hit1"));
+			
+			//Assert.assertEquals(exp, act);
+			
+			//Assert.assertEquals(fetchvaluefromjson.valuefromjson(responseString,"hit1"), fetchvaluefromjson.valuefromjson(req_payLoad,"hit1"));
+
+	}
+
 
 }
--- Cucumberapifw/src/test/java/com/restapibase/Baseapiclass.java
+++ Cucumberapifw/src/test/java/com/restapibase/Baseapiclass.java
@@ -20,9 +20,14 @@ public class Baseapiclass {
 		Response response =requestSpecification.post(requestURI);
 		return response;
 		}
+	public static Response putRequest(String requestURI, String requestpayLoad){
+		RequestSpecification requestSpecification = RestAssured.given().body(requestpayLoad);
+		requestSpecification.contentType(ContentType.JSON);
+		Response response =requestSpecification.put(requestURI);
+		return response;
+		}
 	
-	
-	public static Response postRequest1(String requestURI,String requestpayLoad,String bearer_token){
+	public static Response postRequest(String requestURI,String requestpayLoad,String bearer_token){
 		RequestSpecification requestSpecification = RestAssured.given().body(requestpayLoad).urlEncodingEnabled(false);
 		requestSpecification.contentType(ContentType.JSON);
 		requestSpecification.header("Authorization","Bearer"+bearer_token);
